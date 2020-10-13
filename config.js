ba8a0273fc984bffa6882b54d9c480d477d402f9
yixinjiecode <69457485+yixinjiecode@users.noreply.github.com>
2020-10-13 10:15:09 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 ﻿/*
 Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
+For licensing, see LICENSE.html or http://ckfinder.com/license
 */
 
-CKEDITOR.editorConfig = function(config)
+CKFinder.customConfig = function( config )
 {
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
-    // config.uiColor = '#AADC6E';
-    CKFinder.setupCKEditor(null, '../ckfinder/'); // 在 CKEditor 中集成 CKFinder，注意 ckfinder 的路径选择要正确。
+	// Define changes to default configuration here. For example:
+	// config.skin = 'v1';
+	// config.language = 'fr';
 };
